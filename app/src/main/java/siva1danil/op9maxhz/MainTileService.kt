db88c89c2deb494b9acd79af80d23d663ea7b42b
siva1danil <siva1danil@ya.ru>
2025-10-11 20:17:57 +0700

    Open the app on long press qs tile

--- app/src/main/java/siva1danil/op9maxhz/MainTileService.kt
+++ app/src/main/java/siva1danil/op9maxhz/MainTileService.kt
@@ -6,7 +6,7 @@ import android.service.quicksettings.TileService
 import androidx.annotation.RequiresApi
 
 @RequiresApi(Build.VERSION_CODES.N)
-class MaxHzTileService : TileService() {
+class MainTileService : TileService() {
     private lateinit var _repository: RefreshRateRepository
 
     override fun onCreate() {
